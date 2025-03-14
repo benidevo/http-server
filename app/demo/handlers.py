e26ba6f03242639cee1915229ce8e7e8cff299da
Benjamin Idewor <benjaminidewor@gmail.com>
2025-03-14 19:09:52 +0100

    Read header

--- app/demo/handlers.py
+++ app/demo/handlers.py
@@ -10,5 +10,13 @@ logger = logging.getLogger(__name__)
 
 class EchoHandler(BaseHandler):
     def get(self, request: Request) -> Response:
-        path = request.metadata.path_params.get("str") or "Hello, World!"
-        return Response(status=Status.OK, body=path)
+        path = request.metadata.path_params.get("str", "Hello, World!")
+        return Response(body=path)
+
+
+class UserAgentHandler(BaseHandler):
+    def get(self, request: Request) -> Response:
+        user_agent = request.headers.get(
+            "User-Agent", request.headers.get("user-agent", "Unknown")
+        )
+        return Response(body=user_agent)
--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from app.configs import settings
-from app.demo.handlers import EchoHandler
+from app.demo.handlers import EchoHandler, UserAgentHandler
 from app.server import HttpServer
 
 
 def main() -> None:
     server = HttpServer(settings.host, settings.port)
     server.router.add_route("/echo/{str}", EchoHandler)
+    server.router.add_route("/user-agent", UserAgentHandler)
     server.run()
 
 
